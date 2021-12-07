b07c46fe781d8e66bdb6dbfa38b6aee9f39dd9c3
GigioDC69 <69433647+GigioDC69@users.noreply.github.com>
2021-12-07 13:01:47 -0300

    Update sem-estoque.php

--- sem-estoque.php
+++ sem-estoque.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
     <title>Sem estoque</title>
 </head>
-<body style="background-color:red;">
+<body class="gradient"style="background-color:red;">
     <h1 style="font-size:50px;"><b>OPS! este produto está fora do estoque.</b></h1>
     <h2><a href="index.php"><b>voltar</b></a></h2>
 </body>
