f51be349983fa092c58f8da03f20118368f47a96
arth347 <arthgamer13@gmail.com>
2021-12-07 14:14:06 -0300

    uptate da tela sem estoque

--- sem-estoque.php
+++ sem-estoque.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
     <title>Sem estoque</title>
 </head>
-<body style="background-color:red;">
-    <h1 style="font-size:50px;"><b>OPS! este produto está fora do estoque.</b></h1>
-    <h2><a href="index.php"><b>voltar</b></a></h2>
+<body class="gradient">
+    <div class="container-gigiodece">
+        <h1 style="font-size:50px; font-family:fantasy;"><b>OPS! este produto está fora do estoque.</b></h1>
+        <h2 id="gigiodc"><a href="index.php">voltar</a></h2>
+    </div>
 </body>
 </html>
+
+
